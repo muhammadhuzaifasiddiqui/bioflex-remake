c6507feee1e5fcf8a9243f7a401920117635ac47
Huzaifa <huzaifasiddiquiep@gmail.com>
2025-08-26 13:59:06 +0500

    add

---
+++ src/app/pages/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from ;
+import Image from "next/image";
+
+const page = () => {
+  return (
+    <>
+    <navbar />
+    </>
+  );
+};
+
+export default page;
